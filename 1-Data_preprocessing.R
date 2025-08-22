dec0dcd80fed3105da76a4dffa5493eaee7a875c
Jean Morrison <jvmorr@umich.edu>
2025-08-22 10:56:55 -0400

    Refactor data loading to use snakemake inputs

--- 1-Data_preprocessing.R
+++ 1-Data_preprocessing.R
@@ -2,49 +2,56 @@ library(glmmTMB)
 library(dplyr)
 library(readxl)
 library(haven)
-print(1)
-setwd("/net/orion/skardia_lab/clubhouse/research/projects/LASI/morrison_lab/20250528_EWAS1")
-args <- commandArgs(trailingOnly = TRUE)
-array_id <- as.numeric(args[1])
-print(2)
+
+
 
 adbio_col = c("w1abeta40_final", "w1abeta42_final", "w1gfap_final", "w1nfl_final", "w1ptau_final", "w1totaltau_final")
-adbio_gen <- read_dta("/net/orion/skardia_lab/clubhouse/research/projects/LASI/Alzheimers_Disease/Updated LASI-DAD AD biomarker data/lasidad_w12adbio_final_genomicID.dta")
-adbio_gen <- adbio_gen %>% filter(inw1_adbio == 1) %>% 
-  dplyr::select(newid, w1abeta40_final, w1abeta42_final, w1gfap_final, w1nfl_final, w1ptau_final, w1totaltau_final) %>%
-  filter(!is.na(w1abeta40_final) & newid != "")
-adbio_meth <- read_dta("/net/orion/skardia_lab/clubhouse/research/projects/LASI/Alzheimers_Disease/Updated LASI-DAD AD biomarker data/lasidad_w12adbio_final_methylID.dta")
+
+adbio_meth <- read_dta(snakemake@input[["biomarker"]])
 adbio_meth <- adbio_meth %>% filter(inw1_adbio == 1 & barcode_w1 != "") %>% 
   dplyr::select(barcode_w1, w1abeta40_final, w1abeta42_final, w1gfap_final, w1nfl_final, w1ptau_final, w1totaltau_final) %>%
   filter(!if_all(all_of(adbio_col), is.na)) %>%
   mutate(Sample_name = barcode_w1)
 
-load('/net/orion/skardia_lab/clubhouse/research/projects/LASI/Methylation_Nov14_2024/Scott_QC_test/LASI_betas_filtered_928074x2340_beadlt4NA_best_detP_Reps.RData')
-load('/net/orion/skardia_lab/clubhouse/research/projects/LASI/Methylation_Nov14_2024/Scott_QC_test/Modified_EPICv2_manifest_in_TJ.Peters_BMCgenomics_2024.rda')
-HRS_Gap_Probes_v1 <- read_excel("HRS Gap Probes v1.xlsx")
+# loads a matrix called beta2
+# load(snakemake@input[["beta"]])
+
+#loads Modified_EPICv2_manifest_in_TJ.Peters_BMCgenomics_2024 which is a dataframe
+load(snakemake@input[["manifest"]])
+HRS_Gap_Probes_v1 <- read_excel(snakemake@input[["gap"]])
 
 # Exclude CpGs on chr X, chr Y and M
-manifest = Modified_EPICv2_manifest_in_TJ.Peters_BMCgenomics_2024[,c(9,10,12)]
+manifest = Modified_EPICv2_manifest_in_TJ.Peters_BMCgenomics_2024[,c(9,10,12)] 
 manifest$CpG = rownames(manifest)
-rm(Modified_EPICv2_manifest_in_TJ.Peters_BMCgenomics_2024)
 colnames(manifest) = c('Chr', 'Position', 'Gene', 'CpG')
 manifest_excludeX = manifest %>% filter(Chr != 'chrX' & Chr != 'chrY' & Chr != 'chrM') %>%
   mutate(CPG = sub("_.*", "", CpG)) %>%
   filter(!(CPG %in% HRS_Gap_Probes_v1$CPG))
 
-methx = beta2[rownames(beta2) %in% manifest_excludeX$CpG,]
-rm(beta2)
-methy = data.frame(sample=colnames(methx), t(methx), row.names = NULL)
-rm(methx)
-cpg = colnames(methy)[-1]
- 
-load("/net/orion/skardia_lab/clubhouse/research/projects/LASI/morrison_lab/20250528_EWAS1/Methylation.Rda")
-Meth = t(Meth[rownames(Meth) %in% cpg,])
-Meth = data.frame(sample=rownames(Meth), Meth, row.names = NULL)
-Unmeth = t(Unmeth[rownames(Unmeth) %in% cpg,])
-Unmeth = data.frame(sample=rownames(Unmeth), Unmeth, row.names = NULL)
 
-load(file='/net/orion/skardia_lab/clubhouse/research/projects/LASI/Methylation_Nov14_2024/Scott_QC_test/LASI_phenos_2340.RData')
+# loads matrix Meth and Unmeth
+load(snakemake@input[["meth"]])
+cpgs <- manifest_excludeX$CpG
+keep_meth_ix <- which(rownames(Meth) %in% cpgs)
+keep_cpgs <- rownames(Meth)[keep_meth_ix]
+samples = colnames(Meth)
+
+stopifnot(all(rownames(Meth) == rownames(Unmeth)))
+stopifnot(all(colnames(Meth) == colnames(Unmeth)))
+
+# TODO: Check that betas derived from Meth and Unmeth match beta2 from Scott
+Meth = t(Meth[keep_meth_ix,])
+Meth = data.frame(sample=samples, Meth, row.names = NULL)
+names(Meth) <- paste0(keep_cpgs, "_M")
+
+
+Unmeth = t(Unmeth[keep_meth_ix,])
+Unmeth = data.frame(sample=samples, Unmeth, row.names = NULL)
+names(Unmeth) <- paste0(keep_cpgs, "_U")
+
+
+#loads data frame phenos
+load(file=snakemake@input[["pheno"]])
 phenos = phenos[which(phenos$wave=='wave1'),]
 phenos$MedGenome_Sample_ID = as.numeric(phenos$MedGenome_Sample_ID)
 colnames(phenos)[3] = "sample"
@@ -58,13 +65,15 @@ phenos$smoke = as.factor(
 phenos = phenos %>% mutate(ragender = case_when(ragender == "1.Man" ~ 0, ragender == "2.Woman" ~ 1)) %>%
   inner_join(adbio_meth, by = c("Sample_name"))
 
-wbc = read.csv('/net/orion/skardia_lab/clubhouse/research/projects/LASI/Methylation_Nov14_2024/Scott_QC_test/LASI_WBC.csv')
-phenos = merge(phenos, wbc, by='sample')
-phenos = phenos[-which(duplicated(phenos$MedGenome_Sample_ID)),]
-lasi = merge(methy, phenos, by='sample')
-lasi_meth = merge(Meth, phenos, by = 'sample')
-lasi_unmeth = merge(Unmeth, phenos, by = 'sample')
+wbc = read.csv(snakemake@input[["wbc"]])
 
-pheno_data = lasi[,(ncol(lasi)-100):ncol(lasi)]
-save(cpg, lasi, lasi_meth, lasi_unmeth, manifest_excludeX, file = "/net/orion/skardia_lab/clubhouse/research/projects/LASI/morrison_lab/20250813_EWAS3/all_data.Rda")
-save(pheno_data,  file = "/net/orion/skardia_lab/clubhouse/research/projects/LASI/morrison_lab/20250813_EWAS3/pheno_data.Rda")
+phenos = inner_join(phenos, wbc, by='sample') %>%
+         inner_join(Meth, by = "sample") %>%
+         inner_join(Unmeth, by = "sample")
+
+# TODO: Find out what is going on with duplicates? Are they the same data twice or different data?
+phenos = phenos[-which(duplicated(phenos$MedGenome_Sample_ID)),]
+
+saveRDS(phenos, file = snakemake@output[["merged_data"]])
+saveRDS(keep_cpgs, file = snakemake@output[["cpg_list"]])
+saveRDS(manifest_excludeX, file = snakemake@output[["manifest"]])
